80e4b10f25bab19b1a2f1d1d9093632cc7290fba
Jefferson Huisa <j3ffh95@gmail.com>
2022-10-18 23:43:56 -0400

    making sure that data of the top songs are loading correctly

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -5,8 +5,14 @@ import { genres } from "../assets/constants";
 import { useGetTopChartsQuery } from "../redux/services/shazamCore";
 
 const Discover = () => {
-  console.log(genres);
+  const { data, isFetching, error } = useGetTopChartsQuery();
   const genreTitle = "Pop";
+
+  if (isFetching) return <Loader title='Loading songs....' />;
+  if (error) return <Error />;
+
+  // console.log(data);
+
   return (
     <div className='flex flex-col'>
       <div
@@ -30,7 +36,7 @@ const Discover = () => {
         </select>
       </div>
       <div className='flex flex-wrap sm:justify-start justify-center gap-8'>
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((song, i) => (
+        {data?.map((song, i) => (
           <SongCard key={song.key} song={song} i={i} />
         ))}
       </div>
